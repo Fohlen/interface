84fe16fe260dbba90018695916afe3d9f9347947
MartinMuzatko <martin@happy-css.com>
2019-01-11 00:08:00 +0100

    added entities page and migrated to stylus

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,9 @@
         <nuxt/>
     </div>
 </template>
+
+<style lang="stylus">
+    html
+        background-image url("~assets/averas.jpg")
+        background-size cover
+</style>
--- pages/editor/entities/index.vue
+++ pages/editor/entities/index.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="bg-background text-white p-4 m-auto mt-16 w-3/4">
         <h1>Inexor</h1>
-        <nuxt-link to="editor/entities">Entities</nuxt-link>
     </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-    components: {
-        Logo
-    }
+
 }
 </script>
+
+<style>
+
+</style>
