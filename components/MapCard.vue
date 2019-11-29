0bcb98545fb4949629185a6af31aea7602463347
Martin Muzatko <martin@happy-css.com>
2019-11-29 23:32:04 +0100

    add UserAvatar and MapCard component

---
+++ components/MapCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="p-2 relative overflow-hidden">
+        <div class="bg-background transition hover:scale relative map flex">
+            <img :src="map.preview" alt="" class="absolute pin-t pin-l w-full h-full">
+            <div class="bg-background p-2 mt-16 self-end relative w-full">
+                <strong class="block text-purple text-shadow-dark">{{ map.name }}</strong>
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        map: {
+            type: Object,
+            default: () => ({
+                name: '',
+            })
+        },
+    }
+}
+</script>
+
+<style lang="stylus">
+    .map
+        min-height 5em
+</style>
---
+++ components/user/UserAvatar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex items-center">
+        <img :class="getSize" class="avatar" :src="user.avatar">
+        <div v-if="label" class="ml-2">
+            <div class="font-bold text-purple text-shadow-dark">{{ user.username }}</div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: Object,
+        label: Boolean,
+        small: Boolean,
+        big: Boolean,
+    },
+    computed: {
+        getSize() {
+            return {
+                'h-8 w-8': this.small,
+                'h-16 w-16': !this.small && !this.big,
+                'h-32 w-32': this.big,
+            }
+        }
+    }
+}
+</script>
